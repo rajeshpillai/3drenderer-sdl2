f152f06fdbc5e4c9e5ab78921814e399a3d3f274
Rajesh Pillai <rajesh@algorisys.com>
2023-02-06 10:21:05 +0530

    Create and initialize window and renderer with a boilerplate for game loop

--- src/main.c
+++ src/main.c
@@ -1,7 +1,67 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
 
 
-int main (void) {
-  printf("Hello World");
-  return 0;
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool initialize_window(void) {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    fprintf(stderr, "Error initializing SDL.\n");
+    return false;
+  }
+
+// Create a SDL Window
+  window = SDL_CreateWindow(
+    NULL,
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    800,
+    600,
+    SDL_WINDOW_BORDERLESS
+  );
+
+   if (NULL == window) {
+    fprintf(stderr, "Error creating SDL window.\n");
+    return false;
+  }
+
+  // Create a SDL renderer
+  renderer = SDL_CreateRenderer(window, -1, 0);
+  if (!renderer) {
+    fprintf(stderr, "Error creating SDL renderef.\n");
+    return false;
+  }
+  return true;
 }
+
+void setup(void) {
+
+}
+
+void process_input(void) {
+  
+}
+
+void update(void) {
+
+}
+
+void render(void) {
+  
+}
+
+int main (int argv, char** args) {
+  is_running = initialize_window();
+
+  setup();
+  while(true) {
+    process_input();
+    update();
+    render();
+  }
+
+  return 0;
+} 
