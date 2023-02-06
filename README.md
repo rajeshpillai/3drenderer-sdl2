10dcaa3c5195a23f9c174796fbb09dade115c1c0
Rajesh Pillai <rajesh@algorisys.com>
2023-02-06 11:19:52 +0530

    Declaring color buffer, allocating memroy and cleanup

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ A crossplatform graphics library using SDL2
 
 ## Compile executable
 ```mingw32-make```
+
+# Features
+- Setup SDL2
+- Create and Render a SDL Window
+- Game Loop
+- Declaring Color Buffer and allocating memory
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>  // uintx_t types
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
@@ -6,6 +7,10 @@
 bool is_running = false;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+
+uint32_t* color_buffer = NULL;
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -18,8 +23,8 @@ bool initialize_window(void) {
     NULL,
     SDL_WINDOWPOS_CENTERED,
     SDL_WINDOWPOS_CENTERED,
-    800,
-    600,
+    window_width,
+    window_height,
     SDL_WINDOW_BORDERLESS
   );
 
@@ -38,7 +43,8 @@ bool initialize_window(void) {
 }
 
 void setup(void) {
-
+  // The programmer is responsible for clearing the memory allocated with malloc.
+  color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
 }
 
 void process_input(void) {
@@ -62,6 +68,14 @@ void update(void) {
 
 }
 
+// Clean up
+void destroy_window(void) {
+  free(color_buffer);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
+
 void render(void) {
   SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255 );
   SDL_RenderClear(renderer);
@@ -78,5 +92,6 @@ int main (int argv, char** args) {
     render();
   }
 
+  destroy_window();
   return 0;
 } 
